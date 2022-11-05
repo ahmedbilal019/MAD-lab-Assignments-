dbd0f4a6aa5ac4e47b75ee322867eed7b3fdab99
Ahmed Bilal <ahmed258bilal@gmail.com>
2022-11-05 21:36:49 +0500

    Theory Assignment 2
    Ludo Design
    Ahmed Bilal

---
+++ Theory Assignment 2/App.js
@@ -0,0 +1,17 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import Ludoboxs from './component/Ludoboxs';
+export default function App() {
+  return (
+    <View>
+       <Ludoboxs/>
+    </View>
+   
+  )
+}
+
+const styles = StyleSheet.create({
+  
+ 
+  
+});
